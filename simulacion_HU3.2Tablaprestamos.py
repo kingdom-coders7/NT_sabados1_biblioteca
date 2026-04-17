8d775436eac0e752aa4fb517a122246195ea663a
JuanDavila12 <jdavilabe12@gmail.com>
2026-04-17 17:36:37 -0500

    se hace pull request para traer los cambios del proyecto

--- simulacion_HU3.2Tablaprestamos.py
+++ simulacion_HU3.2Tablaprestamos.py
@@ -13,5 +13,22 @@ def simular_prestamos(numeroPrestamos):
             "fecha_prestamo": fake.date_between(start_date='-1y', end_date='today'),
             "fecha_devolucion": fake.date_between(start_date='today', end_date='+30d')
         }
+
+        # Inyectar errores controlados
+        probabilidad_error = random.random()
+        if probabilidad_error < 0.1:  # 10% de probabilidad de generar un error
+            prestamo["id_users"] = None  # ID de usuario no válido
+            prestamo["id_libro"] = prestamo["id_libro"].lower()  # ID de libro no válido
+        elif probabilidad_error < 0.2:  # 20% de probabilidad de generar otro error
+            prestamo["fecha_prestamo"] = None  # Fecha de préstamo no válida
+        elif probabilidad_error < 0.3:
+            prestamo["fecha_devolucion"] = None
+            prestamo["id_prestamo"] = random.choice([None, -1, 0])  # Fecha de devolución no válida
+        elif probabilidad_error < 0.6:
+            prestamo["id_libro"] = None 
+            prestamo["id_prestamo"] = prestamo["id_prestamo"]="texto "," ";"@" 
+            # ID de libro no válido    
         prestamos.append(prestamo)
     return prestamos
+
+#pendiente realizar el pull request para agregar este codigo al repositorio
