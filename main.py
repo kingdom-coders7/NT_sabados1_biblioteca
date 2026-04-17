515318bd0e2fa1dbfc14f1abe0554f99485d0aa0
Santiago Román <santi19r@gmail.com>
2026-04-16 19:39:39 -0500

    fix(se reemplazan nombres de los archivos)

---
+++ main.py
@@ -0,0 +1,9 @@
+from simulacion_HU3_5_tabla_libros import simular_libros
+from simulacion_HU3_6_tabla_stand import simular_stands
+
+simulaciones = simular_stands(1)
+print(simulaciones)
+
+simulaciones = simular_libros(1)
+print(simulaciones)
+
--- simulacion_HU3_6_tabla_stand.py
+++ simulacion_HU3_6_tabla_stand.py
@@ -16,5 +16,13 @@ def simular_stands(numeroStands):
             "id_stand": random.choice(listaStands),
             "location":random.choice(listaLocacion),
         }
+
+        #Inyectando errores controlados
+        probabilidadError = random.random()
+        if probabilidadError < 0.25:
+            stand["id_stand"] = None
+        elif probabilidadError < 0.5:
+            stand["location"] = random.choice(["La cocina", "carniceria"])
         stands.append(stand)
-        return stands
+        
+    return stands
